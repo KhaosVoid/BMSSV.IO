330e9c68913853452e99e9a8fcd87701dc327f6e
KhaosVoid <ztommey@gmail.com>
2021-11-01 04:06:54 -0500

    Updated TileTypes enum to reflect named values

--- src/BMSSV.IO/MetroidDread/Types/TileTypes.cs
+++ src/BMSSV.IO/MetroidDread/Types/TileTypes.cs
@@ -8,15 +8,16 @@ namespace BMSSV.IO.MetroidDread.Types
 {
     public enum TileTypes
     {
-        //Type0 = 0,
-        Type1 = 1,
-        Type2 = 2,
-        Type3 = 3,
-        //Type4 = 4,
-        Type5 = 5,
-        Type6 = 6,
-        Type7 = 7,
-        //Type8 = 8,
-        Type9 = 9
+        Undefined = 0,
+        Beam = 1,
+        Bomb = 2,
+        Missle = 3,
+        SuperMissile = 4,
+        PowerBomb = 5,
+        ScrewAttack = 6,
+        Crumble = 7,
+        BabyHatchling = 8,
+        SpeedBoost = 9,
+        Invalid = int.MaxValue
     }
 }

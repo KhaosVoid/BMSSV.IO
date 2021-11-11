fa9982a1e5c965049dfef33a6c02ebff7585c9f1
KhaosVoid <ztommey@gmail.com>
2021-11-10 18:34:27 -0600

    Added remaining OccluderColliderTypeIds

--- src/BMSSV.IO/MetroidDread/Enums.cs
+++ src/BMSSV.IO/MetroidDread/Enums.cs
@@ -60,73 +60,130 @@ namespace BMSSV.IO.MetroidDread.Enums
 
     public enum OccluderColliderTypeIds : ulong
     {
+        //blockforcemovement
+        BlockForceMovement   = 0xBC11E1CCA81387BF,
+
         //collision
-        Collision          = 0x256582A39FB5119F,
+        Collision            = 0x256582A39FB5119F,
 
-        //trapblockfinal
-        TrapBlockFinal     = 0x7BABC0C8085511C2,
-
-        //collision_opened
-        CollisionOpened    = 0x2C15B41771891777,
-
-        //collision_close
-        CollisionClose     = 0xC1A3AFE60BD47C80,
-
-        //collision_closed
-        CollisionClosed    = 0x2F6D2F820BE625FA,
-
-        //collision_box
-        CollisionBox       = 0x1A9241A8743F4CAB,
-
-        //collisions
-        Collisions         = 0xE59D9C72946EE696,
-
-        //collision_base
-        CollisionBase      = 0x1ED58C7641EFCC2C,
-
-        //collision_up
-        CollisionUp        = 0xEC6670556EB06377,
-
-        //collision012_DEBRIS
-        Collision012DEBRIS = 0xDBEC65DF4FDE1A6D,
-
-        //collision007_DEBRIS
-        Collision007DEBRIS = 0x6FF3E71C57D6839C,
-
-        //collision018
-        Collision018       = 0x1DCF8BB14CF6E63A,
-
-        //collision2
-        Collision2         = 0x2B45F89AE0097E41,
-
-        //collision_hit
-        CollisionHit       = 0xAFE3B5CB55A6025B,
+        //collision001
+        Collision001         = 0x19DDE2116944802A,
 
         //collision003
-        Collision003       = 0x427DA2B93E7204DE,
+        Collision003         = 0x427DA2B93E7204DE,
 
         //collision006
-        Collision006       = 0x1E7C8A4504FDFA16,
+        Collision006         = 0x1E7C8A4504FDFA16,
+
+        //collision007_DEBRIS
+        Collision007DEBRIS   = 0x6FF3E71C57D6839C,
+
+        //collision008
+        Collision008         = 0x103E5BEDDE8F0F6E,
+
+        //collision009
+        Collision009         = 0x7F61FCEE60C321DD,
+
+        //collision010
+        Collision010         = 0x7B73954E457147CD,
+
+        //collision012
+        Collision012         = 0x20D3D5E61247C339,
+
+        //collision012_DEBRIS
+        Collision012DEBRIS   = 0xDBEC65DF4FDE1A6D,
+
+        //collision018
+        Collision018         = 0x1DCF8BB14CF6E63A,
 
         //collision020
-        Collision020       = 0x6C60E4ABF3FB7C30,
+        Collision020         = 0x6C60E4ABF3FB7C30,
 
-        //UnknownType1
-        UnknownType1       = 0xD0742138FC74EC08,
+        //collision2
+        Collision2           = 0x2B45F89AE0097E41,
 
-        //UnknownType2
-        UnknownType2       = 0x145D990A588908BB,
+        //collision_base
+        CollisionBase        = 0x1ED58C7641EFCC2C,
 
-        //UnknownType3
-        UnknownType3       = 0xE48BC693150E464F,
+        //collision_base_action1
+        CollisionBaseAction1 = 0x1137ADBCDF47C48F,
 
-        //UnknownType4
-        UnknownType4       = 0x40BAA540D530AA25,
+        //collision_base_action2
+        CollisionBaseAction2 = 0x25C84A17363D6EC8,
 
-        //UnknownType5
-        UnknownType5       = 0x8DC2E7510FAB1F45,
+        //collision_base_action3
+        CollisionBaseAction3 = 0x4A97ED148871407B,
 
-        //UnknownType6
-        UnknownType6       = 0x5A5804F9F1C3788C
+        //collision_box
+        CollisionBox         = 0x1A9241A8743F4CAB,
+
+        //collision_close
+        CollisionClose       = 0xC1A3AFE60BD47C80,
+
+        //collision_closed
+        CollisionClosed      = 0x2F6D2F820BE625FA,
+
+        //collision_hidden
+        CollisionHidden      = 0x8E69AF17CBF9216D,
+
+        //collision_hit
+        CollisionHit         = 0xAFE3B5CB55A6025B,
+
+        //collision_joint
+        CollisionJoint       = 0x8DC2E7510FAB1F45,
+
+        //collision_open
+        CollisionOpen        = 0x145D990A588908BB,
+
+        //collision_opened
+        CollisionOpened      = 0x2C15B41771891777,
+
+        //collision_stage1
+        CollisionStage1      = 0xD0742138FC74EC08,
+
+        //collision_stage2
+        CollisionStage2      = 0xE48BC693150E464F,
+
+        //collision_up
+        CollisionUp          = 0xEC6670556EB06377,
+
+        //collision_up_action1
+        CollisionUpAction1   = 0x6A9B035C515B88AA,
+
+        //collision_up_action2
+        CollisionUpAction2   = 0x5E64E4F7B82122ED,
+
+        //collision_up_action3
+        CollisionUpAction3   = 0x313B43F4066D0C5E,
+
+        //collisions
+        Collisions           = 0xE59D9C72946EE696,
+
+        //door
+        Door                 = 0xA4FA8206F8950523,
+
+        //doorcooldown
+        DoorCooldown         = 0x734620C223FEACF8,
+
+        //navmesh_1m
+        Navmesh1m            = 0x5A5804F9F1C3788C,
+
+        //navmesh_2m
+        Navmesh2m            = 0x4D4B751C47494371,
+
+        //navmesh_3m
+        Navmesh3m            = 0x40BAA540D530AA25,
+
+        //navmesh_4m
+        Navmesh4m            = 0xE673997901F3EC19,
+
+        //navmeshweight
+        NavmeshWeight        = 0x76CF85DDFDBEDC77,
+
+        //trapblockfinal
+        TrapBlockFinal       = 0x7BABC0C8085511C2,
+
+        //trapblockinitial
+        TrapBlockInitial     = 0xB0D1AA0C6BF62534
     }
 }
